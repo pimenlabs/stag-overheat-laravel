f000533b7d6b4cfbf69ed58457a3746ec864c170
ega <radiegtya@yahoo.co.id>
2017-02-16 23:34:21 +0700

    bab 12 done

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,23 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('question', function(){
+  echo "this is question page";
+});
+
+Route::get('question/{id}', function($id){
+  echo "get question with id = ". $id;
+});
+
+// Route::post('question', function(){
+//   echo "store question with object from POST data";
+// });
+//
+// Route::put('question/{id}', function($id){
+//   echo "update question with id = ". $id. " and object from PUT data";
+// });
+//
+// Route::delete('question/{id}', function($id){
+//   echo "delete question with id = ". $id;
+// });
